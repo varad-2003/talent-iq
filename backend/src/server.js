c487add5203a512f249d703c5e9535cff219b246
varad-2003 <vashushukla31@gmail.com>
2025-12-20 01:42:38 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,29 @@
 import express from "express";
+import path from "path";
 import { ENV } from "./lib/env.js";
 
 const app = express()
 const port = ENV.PORT
 
-app.get("/", (req, res) => {
+const __dirname = path.resolve();
+
+app.get("/books", (req, res) => {
     res.status(200).json({msg:"hello from talent-iq"})
 })
+app.get("/health", (req, res) => {
+    res.status(200).json({msg:"talent-iq is up and running"})
+})
 
-app.listen(port, () => {
+// make our app ready for deployment
+
+if(ENV.NODE_ENV === "development"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
+
+app.listen(ENV.PORT, () => {
     console.log(`server is running on port ${port}`);
-    
 })
